f3399ff536ddf7eee80af20e76cacfbfde2a7018
RUBY\Ruby Simpson <rubysimpsonwork@gmail.com>
2016-11-24 12:50:15 +1000

    Jarrod has added some scripts for pause and ui

---
+++ New Unity Project/Assets/Loadlevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Loadlevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+	public void UILoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+	
+}
---
+++ New Unity Project/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+    public GameObject pauseButton;
+
+	public void Pause()
+    {
+        Time.timeScale = 0;
+        
+    } 
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+    }   
+}
