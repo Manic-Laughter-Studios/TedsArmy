b81efbbfd3814343f8d88fd493d14f75a69404ba
Jarrod Whittaker <jwhittaker4@sae.au>
2016-11-24 14:10:12 +1000

    Models and prefabs added, pause works and new main menu and credits that buttons work

---
+++ New Unity Project/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour
+{
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
